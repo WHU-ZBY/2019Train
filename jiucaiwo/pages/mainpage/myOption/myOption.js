3160f43e447118d8e5454dc362d2fc6eb8bdd26a
AndyFool <1019804286@qq.com>
2019-06-25 11:32:08 +0800

    小程序1
    小程序1

---
+++ jiucaiwo/pages/mainpage/myOption/myOption.js
@@ -0,0 +1,67 @@
+// pages/mainpage/myOption/myOption.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    stockIdexs: [{ name: "上证指数",num:"2300", present: "-3.7", forecast: "+2.0" }, { name: "恒生指数", num:"20340",present: "-3.3", forecast: "+9.0" }, { name: "道琼斯",num:"23423", present: "+1.7", forecast: "+3.0"}],
+    test : "3"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
